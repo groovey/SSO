3ef4efbfdd90fb7dbe8dc5f4a3b3320f257ba018
pokoot <pokoot@gmail.com>
2017-10-11 03:06:47 +0800

    New migration format

--- tests/Database.php
+++ tests/Database.php
@@ -13,7 +13,7 @@ class Database
 
     public static function drop($app)
     {
-        $app['tester']->command('migrate:down')->input('Y\n')->execute(['version' => '001'])->display();
+        $app['tester']->command('migrate:down')->input('Y\n')->execute(['version' => '0.0.1'])->display();
         $app['tester']->command('migrate:drop')->input('Y\n')->execute()->display();
     }
 }
